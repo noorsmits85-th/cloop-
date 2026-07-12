6856e9e7457ae5ce341bde8fc6de66e5d986c2f3
noorsmits85-th <noorsmits85@gmail.com>
2026-07-13 00:09:32 +0700

    fix: change prisma engine to classic

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from '@prisma/config';
 import "dotenv/config"; // 🚀 NẠP THƯ VIỆN ĐỂ BỐC TỰ ĐỘNG LINK SUPABASE THẬT
 
 export default defineConfig({
+  engine: 'classic', // ✨ THÊM DÒNG NÀY VÀO ĐỂ SỬA LỖI HIỆN TẠI NÈ TRANG
   datasource: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL || '',
   },
 });
