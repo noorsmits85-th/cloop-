84a575c0cc12bf4dc8d6be11226425bc02cb8e8e
noorsmits85-th <noorsmits85@gmail.com>
2026-08-15 00:54:19 +0700

    Fix duplicate /upload route

--- app/upload/page.tsx
+++
@@ -1,9 +0,0 @@
-import UploadForm from "@/components/forms/UploadForm";
-
-export default function UploadPage() {
-  return (
-    <div className="min-h-screen bg-stone-50 py-12 px-4 sm:px-6 lg:px-8 pt-24">
-      <UploadForm />
-    </div>
-  );
-}
